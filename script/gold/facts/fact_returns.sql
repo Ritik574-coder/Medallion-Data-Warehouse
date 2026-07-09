be859bcfb3dcca44ad6dc570a0f7c7677bc3689b
ritik574-coder <ritik74820@gmail.com>
2026-07-09 18:10:24 +0530

    adding fact and dim vartual table in gold layer following medallion_stracture and kimball rule

--- script/gold/facts/fact_returns.sql
+++ script/gold/facts/fact_returns.sql
@@ -1,22 +1,6 @@
-CREATE OR ALTER VIEW gold.fact_returns AS
-SELECT
+CREATE OR ALTER VIEW gold.fact_returns AS 
+SELECT 
     return_id,
-    original_txn_id,
-    original_order_id,
-    customer_id,
-    product_id,
-    handled_by_emp_id AS employee_id,
-    DENSE_RANK() OVER (
-        ORDER BY return_reason, refund_method, return_channel, return_status, restocked
-    ) AS return_reason_key,
-    CAST(CONVERT(CHAR(8), return_date, 112) AS INT) AS return_date_key,
     quantity_returned,
-    refund_amount,
-    return_reason,
-    refund_method,
-    return_channel,
-    return_status,
-    restocked,
-    notes
-FROM silver.returns;
-GO
+    refund_amount
+FROM silver.returns ;
